17d9adb180bafaaab2e68f1dbc8750ae2a45d1f0
chatterbox-house <tony@chatterboxhouse.com>
2025-06-24 14:03:46 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+// sw.js - Basic Service Worker
+const CACHE_NAME = 'jap-learner-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  // Add other assets you want to cache
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
